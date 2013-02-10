3cad8a0b65105c5f9947e45e725ba13dddb8d35a
Stuart McCulloch <mcculls@gmail.com>
2013-02-10 23:27:16 +0000

    Treat adders like setters (helps with re-use inside plexus configurators)

--- org.eclipse.sisu.inject/src/org/eclipse/sisu/reflect/BeanPropertyIterator.java
+++ org.eclipse.sisu.inject/src/org/eclipse/sisu/reflect/BeanPropertyIterator.java
@@ -10,7 +10,6 @@
  *******************************************************************************/
 package org.eclipse.sisu.reflect;
 
-import java.lang.reflect.AnnotatedElement;
 import java.lang.reflect.Field;
 import java.lang.reflect.Member;
 import java.lang.reflect.Method;
@@ -66,10 +65,9 @@ final class BeanPropertyIterator<T>
 
             if ( member instanceof Method )
             {
-                final Method method = (Method) member;
-                if ( isSetter( method ) )
+                if ( isSetterOrAdder( member ) )
                 {
-                    nextProperty = new BeanPropertySetter<T>( method );
+                    nextProperty = new BeanPropertySetter<T>( (Method) member );
                 }
             }
             else if ( member instanceof Field )
@@ -102,13 +100,12 @@ final class BeanPropertyIterator<T>
     // Implementation methods
     // ----------------------------------------------------------------------
 
-    private static boolean isSetter( final Method method )
+    private static boolean isSetterOrAdder( final Member member )
     {
-        final String name = method.getName();
-        if ( name.startsWith( "set" ) && name.length() > 3 && Character.isUpperCase( name.charAt( 3 ) ) )
-        {
-            return method.getParameterTypes().length == 1;
-        }
-        return false;
+        final String name = member.getName();
+        return ( name.startsWith( "set" ) || name.startsWith( "add" ) ) //
+            && name.length() > 3
+            && Character.isUpperCase( name.charAt( 3 ) )
+            && ( (Method) member ).getParameterTypes().length == 1;
     }
 }
