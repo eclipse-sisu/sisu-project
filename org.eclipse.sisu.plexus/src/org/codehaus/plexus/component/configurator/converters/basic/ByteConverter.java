ee75e47b871962d8c405ddc119d524a075ff879b
Stuart McCulloch <mcculls@gmail.com>
2013-02-14 01:03:13 +0000

    Bug 390240: whole number converters

---
+++ org.eclipse.sisu.plexus/src/org/codehaus/plexus/component/configurator/converters/basic/ByteConverter.java
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * Copyright (c) 2010, 2013 Sonatype, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Stuart McCulloch (Sonatype, Inc.) - initial API and implementation
+ *******************************************************************************/
+package org.codehaus.plexus.component.configurator.converters.basic;
+
+import org.codehaus.plexus.component.configurator.ComponentConfigurationException;
+
+public class ByteConverter
+    extends AbstractBasicConverter
+{
+    public boolean canConvert( final Class<?> type )
+    {
+        return byte.class.equals( type ) || Byte.class.equals( type );
+    }
+
+    @Override
+    public Object fromString( final String value )
+        throws ComponentConfigurationException
+    {
+        try
+        {
+            return Byte.decode( value );
+        }
+        catch ( final NumberFormatException e )
+        {
+            throw new ComponentConfigurationException( "Cannot convert '" + value + "' to byte", e );
+        }
+    }
+}
---
+++ org.eclipse.sisu.plexus/src/org/codehaus/plexus/component/configurator/converters/basic/IntConverter.java
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * Copyright (c) 2010, 2013 Sonatype, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Stuart McCulloch (Sonatype, Inc.) - initial API and implementation
+ *******************************************************************************/
+package org.codehaus.plexus.component.configurator.converters.basic;
+
+import org.codehaus.plexus.component.configurator.ComponentConfigurationException;
+
+public class IntConverter
+    extends AbstractBasicConverter
+{
+    public boolean canConvert( final Class<?> type )
+    {
+        return int.class.equals( type ) || Integer.class.equals( type );
+    }
+
+    @Override
+    public Object fromString( final String value )
+        throws ComponentConfigurationException
+    {
+        try
+        {
+            return Integer.decode( value );
+        }
+        catch ( final NumberFormatException e )
+        {
+            throw new ComponentConfigurationException( "Cannot convert '" + value + "' to int", e );
+        }
+    }
+}
