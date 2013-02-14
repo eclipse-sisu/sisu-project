e43c2ae98bd29663524c5a151040b28196b7a841
Stuart McCulloch <mcculls@gmail.com>
2013-02-14 00:56:37 +0000

    Bug 390240: string converters

---
+++ org.eclipse.sisu.plexus/src/org/codehaus/plexus/component/configurator/converters/basic/StringBuilderConverter.java
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * Copyright (c) 2010, 2013 Sonatype, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Stuart McCulloch (Sonatype, Inc.) - initial API and implementation
+ *******************************************************************************/
+package org.codehaus.plexus.component.configurator.converters.basic;
+
+public class StringBuilderConverter
+    extends AbstractBasicConverter
+{
+    public boolean canConvert( final Class<?> type )
+    {
+        return StringBuilder.class.equals( type );
+    }
+
+    @Override
+    public Object fromString( final String value )
+    {
+        return new StringBuilder( value );
+    }
+}
