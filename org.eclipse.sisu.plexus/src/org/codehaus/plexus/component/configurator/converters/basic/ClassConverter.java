061ce3d3e9c4a2d065317bf8910c826490e25fd7
Stuart McCulloch <mcculls@gmail.com>
2013-02-14 01:06:51 +0000

    Bug 390240: simple 'Class.forName' converter

---
+++ org.eclipse.sisu.plexus/src/org/codehaus/plexus/component/configurator/converters/basic/ClassConverter.java
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * Copyright (c) 2010, 2013 Sonatype, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Stuart McCulloch (Sonatype, Inc.) - initial API and implementation
+ *******************************************************************************/
+package org.codehaus.plexus.component.configurator.converters.basic;
+
+import org.codehaus.plexus.component.configurator.ComponentConfigurationException;
+
+public class ClassConverter
+    extends AbstractBasicConverter
+{
+    public boolean canConvert( final Class<?> type )
+    {
+        return Class.class.equals( type );
+    }
+
+    @Override
+    public Object fromString( final String value )
+        throws ComponentConfigurationException
+    {
+        try
+        {
+            return Class.forName( value );
+        }
+        catch ( final ClassNotFoundException e )
+        {
+            throw new ComponentConfigurationException( "Cannot convert '" + value + "' to Class", e );
+        }
+        catch ( final LinkageError e )
+        {
+            throw new ComponentConfigurationException( "Cannot convert '" + value + "' to Class", e );
+        }
+    }
+}
