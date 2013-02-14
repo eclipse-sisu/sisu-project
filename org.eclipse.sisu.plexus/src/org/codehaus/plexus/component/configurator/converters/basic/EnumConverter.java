174c88bfe4b707f7d767e7f7e91c4b7f07c22082
Stuart McCulloch <mcculls@gmail.com>
2013-02-14 01:10:38 +0000

    Bug 390240: enum converter

---
+++ org.eclipse.sisu.plexus/src/org/codehaus/plexus/component/configurator/converters/basic/EnumConverter.java
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * Copyright (c) 2010, 2013 Sonatype, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Stuart McCulloch (Sonatype, Inc.) - initial API and implementation
+ *******************************************************************************/
+package org.codehaus.plexus.component.configurator.converters.basic;
+
+import org.codehaus.plexus.component.configurator.ComponentConfigurationException;
+import org.codehaus.plexus.component.configurator.ConfigurationListener;
+import org.codehaus.plexus.component.configurator.converters.AbstractConfigurationConverter;
+import org.codehaus.plexus.component.configurator.converters.lookup.ConverterLookup;
+import org.codehaus.plexus.component.configurator.expression.ExpressionEvaluator;
+import org.codehaus.plexus.configuration.PlexusConfiguration;
+
+public class EnumConverter
+    extends AbstractConfigurationConverter
+{
+    public boolean canConvert( final Class<?> type )
+    {
+        return type.isEnum();
+    }
+
+    @SuppressWarnings( { "unchecked", "rawtypes" } )
+    public Object fromConfiguration( final ConverterLookup lookup, final PlexusConfiguration configuration,
+                                     final Class<?> type, final Class<?> enclosingType, final ClassLoader loader,
+                                     final ExpressionEvaluator evaluator, final ConfigurationListener listener )
+        throws ComponentConfigurationException
+    {
+        if ( configuration.getChildCount() > 0 )
+        {
+            throw new ComponentConfigurationException( "Basic element '" + configuration.getName()
+                + "' must not contain child elements" );
+        }
+
+        Object result = fromExpression( configuration, evaluator );
+        if ( result instanceof String )
+        {
+            try
+            {
+                result = Enum.valueOf( (Class) type, (String) result );
+            }
+            catch ( final RuntimeException e )
+            {
+                throw new ComponentConfigurationException( "Cannot convert '" + result + "' to Enum", e );
+            }
+        }
+        return result;
+    }
+}
