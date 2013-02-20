8077f41f0014527378c1f2fe57dd6a75a0aa0627
Stuart McCulloch <mcculls@gmail.com>
2013-02-20 01:23:31 +0000

    Bug 390240: implement DefaultConverterLookup

--- org.eclipse.sisu.plexus/src/org/codehaus/plexus/component/configurator/converters/lookup/DefaultConverterLookup.java
+++ org.eclipse.sisu.plexus/src/org/codehaus/plexus/component/configurator/converters/lookup/DefaultConverterLookup.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2012 Sonatype, Inc.
+ * Copyright (c) 2010, 2013 Sonatype, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,20 +10,103 @@
  *******************************************************************************/
 package org.codehaus.plexus.component.configurator.converters.lookup;
 
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 import org.codehaus.plexus.component.configurator.ComponentConfigurationException;
 import org.codehaus.plexus.component.configurator.converters.ConfigurationConverter;
+import org.codehaus.plexus.component.configurator.converters.basic.BooleanConverter;
+import org.codehaus.plexus.component.configurator.converters.basic.ByteConverter;
+import org.codehaus.plexus.component.configurator.converters.basic.CharConverter;
+import org.codehaus.plexus.component.configurator.converters.basic.ClassConverter;
+import org.codehaus.plexus.component.configurator.converters.basic.DateConverter;
+import org.codehaus.plexus.component.configurator.converters.basic.DoubleConverter;
+import org.codehaus.plexus.component.configurator.converters.basic.EnumConverter;
+import org.codehaus.plexus.component.configurator.converters.basic.FileConverter;
+import org.codehaus.plexus.component.configurator.converters.basic.FloatConverter;
+import org.codehaus.plexus.component.configurator.converters.basic.IntConverter;
+import org.codehaus.plexus.component.configurator.converters.basic.LongConverter;
+import org.codehaus.plexus.component.configurator.converters.basic.ShortConverter;
+import org.codehaus.plexus.component.configurator.converters.basic.StringBufferConverter;
+import org.codehaus.plexus.component.configurator.converters.basic.StringBuilderConverter;
+import org.codehaus.plexus.component.configurator.converters.basic.StringConverter;
+import org.codehaus.plexus.component.configurator.converters.basic.UriConverter;
+import org.codehaus.plexus.component.configurator.converters.basic.UrlConverter;
+import org.codehaus.plexus.component.configurator.converters.composite.ArrayConverter;
+import org.codehaus.plexus.component.configurator.converters.composite.CollectionConverter;
+import org.codehaus.plexus.component.configurator.converters.composite.MapConverter;
+import org.codehaus.plexus.component.configurator.converters.composite.ObjectWithFieldsConverter;
+import org.codehaus.plexus.component.configurator.converters.composite.PlexusConfigurationConverter;
+import org.codehaus.plexus.component.configurator.converters.composite.PropertiesConverter;
+import org.eclipse.sisu.reflect.Weak;
 
 public final class DefaultConverterLookup
     implements ConverterLookup
 {
+    private static final ConfigurationConverter[] DEFAULT_CONVERTERS = {
+        // optimized ordering...
+        new FileConverter(), //
+        new BooleanConverter(), //
+        new StringConverter(), //
+        new IntConverter(), //
+        new CollectionConverter(), //
+        new ArrayConverter(), //
+        new MapConverter(), //
+        new PropertiesConverter(), //
+        new UrlConverter(), //
+        new UriConverter(), //
+        new DateConverter(), //
+        new EnumConverter(), //
+        new LongConverter(), //
+        new FloatConverter(), //
+        new DoubleConverter(), //
+        new CharConverter(), //
+        new ByteConverter(), //
+        new ShortConverter(), //
+        new ClassConverter(), //
+        new PlexusConfigurationConverter(), //
+        new StringBufferConverter(), //
+        new StringBuilderConverter(), //
+        new ObjectWithFieldsConverter() };
+
+    private final Map<Class<?>, ConfigurationConverter> lookupCache = //
+        Weak.concurrentKeys(); // entries will expire on class unload
+
+    private final List<ConfigurationConverter> registeredConverters =
+        new CopyOnWriteArrayList<ConfigurationConverter>();
+
     public void registerConverter( final ConfigurationConverter converter )
     {
-        throw new UnsupportedOperationException();
+        registeredConverters.add( converter );
     }
 
     public ConfigurationConverter lookupConverterForType( final Class<?> type )
         throws ComponentConfigurationException
     {
-        throw new UnsupportedOperationException();
+        ConfigurationConverter converter = lookupCache.get( type );
+        if ( null != converter )
+        {
+            return converter;
+        }
+        for ( int i = 0; i < registeredConverters.size(); i++ )
+        {
+            converter = registeredConverters.get( i );
+            if ( converter.canConvert( type ) )
+            {
+                lookupCache.put( type, converter );
+                return converter;
+            }
+        }
+        for ( int i = 0; i < DEFAULT_CONVERTERS.length; i++ )
+        {
+            converter = DEFAULT_CONVERTERS[i];
+            if ( converter.canConvert( type ) )
+            {
+                lookupCache.put( type, converter );
+                return converter;
+            }
+        }
+        throw new ComponentConfigurationException( "Cannot find converter for type: " + type );
     }
 }
