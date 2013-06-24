faeec2e8b1de21f419c7855aab6054b9c92f1a84
Stuart McCulloch <mcculls@gmail.com>
2013-06-24 14:23:40 +0100

    Backup extension work

--- org.eclipse.sisu.inject/src/org/eclipse/sisu/launch/Extensions.java
+++ org.eclipse.sisu.inject/src/org/eclipse/sisu/launch/Extensions.java
@@ -15,69 +15,102 @@ import java.lang.reflect.Member;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.sisu.BeanScanning;
 import org.eclipse.sisu.bean.DeclaredMembers;
 import org.eclipse.sisu.bean.DeclaredMembers.View;
 import org.eclipse.sisu.inject.Logs;
 import org.eclipse.sisu.space.ClassSpace;
 import org.eclipse.sisu.space.IndexedClassFinder;
 
-public class Extensions
+final class Extensions
 {
     private final ClassSpace space;
 
-    public Extensions( final ClassSpace space )
+    private final boolean global;
+
+    private Extensions( final ClassSpace space, final boolean global )
     {
         this.space = space;
+        this.global = global;
     }
 
-    public <T> Iterable<T> load( final Class<T> api, final Object... args )
+    public static Extensions from( final ClassSpace space, final BeanScanning scanning )
+    {
+        return new Extensions( space, BeanScanning.GLOBAL_INDEX == scanning );
+    }
+
+    public <T> Iterable<T> of( final Class<T> api, final Object... args )
     {
         final List<T> instances = new ArrayList<T>();
         final String index = "META-INF/services/" + api.getName();
-        for ( final String s : new IndexedClassFinder( index, false ).indexedNames( space ) )
+        for ( final String name : new IndexedClassFinder( index, global ).indexedNames( space ) )
         {
             try
             {
                 @SuppressWarnings( "unchecked" )
-                final Class<T> impl = (Class<T>) space.loadClass( s );
+                final Class<T> impl = (Class<T>) space.loadClass( name );
                 if ( api.isAssignableFrom( impl ) )
                 {
                     instances.add( newInstance( impl, args ) );
                 }
             }
-            catch ( Exception e )
+            catch ( final Exception e )
             {
-                Logs.trace( "Problem loading extension {}", s, e );
+                Logs.trace( "Problem loading: {}", name, e );
             }
-            catch ( LinkageError e )
+            catch ( final LinkageError e )
             {
-                Logs.trace( "Problem loading extension {}", s, e );
+                Logs.trace( "Problem loading: {}", name, e );
             }
         }
         return instances;
     }
 
+    @SuppressWarnings( "unchecked" )
     private static <T> T newInstance( final Class<T> impl, final Object... args )
         throws Exception
     {
+        // filter out constructors with incompatible parameter types
+        final Constructor<T>[] ctors = new Constructor[args.length + 1];
         for ( final Member m : new DeclaredMembers( impl, View.CONSTRUCTORS ) )
         {
-            @SuppressWarnings( "unchecked" )
-            final Constructor<T> ctor = (Constructor<T>) m;
+            Constructor<T> ctor = (Constructor<T>) m;
             final Class<?>[] types = ctor.getParameterTypes();
-            if ( args.length != types.length )
+            if ( types.length <= args.length )
             {
-                continue; // wrong number of parameters
-            }
-            for ( int i = 0; i < types.length; i++ )
-            {
-                if ( !types[i].isInstance( args[i] ) )
+                for ( int i = 0; i < types.length; i++ )
                 {
-                    continue; // incompatible parameters
+                    if ( !types[i].isInstance( args[i] ) )
+                    {
+                        ctor = null;
+                        break;
+                    }
+                }
+                if ( null != ctor )
+                {
+                    ctors[types.length] = ctor;
                 }
             }
-            return ctor.newInstance( args );
         }
-        return impl.newInstance(); // ignore parameters
+
+        // perfect match
+        if ( null != ctors[args.length] )
+        {
+            return ctors[args.length].newInstance( args );
+        }
+
+        // partial match
+        for ( int i = args.length - 1; i > 0; i-- )
+        {
+            if ( null != ctors[i] )
+            {
+                final Object[] tempArgs = new Object[i];
+                System.arraycopy( args, 0, tempArgs, 0, i );
+                return ctors[i].newInstance( tempArgs );
+            }
+        }
+
+        // last resort
+        return impl.newInstance();
     }
 }
