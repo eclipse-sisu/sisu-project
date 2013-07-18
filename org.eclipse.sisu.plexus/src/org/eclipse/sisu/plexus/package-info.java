7239b107e057bb363f358129b0bb2811c4603a3f
Stuart McCulloch <mcculls@gmail.com>
2013-07-18 14:00:04 +0100

    javadoc

--- org.eclipse.sisu.plexus/src/org/eclipse/sisu/plexus/package-info.java
+++ org.eclipse.sisu.plexus/src/org/eclipse/sisu/plexus/package-info.java
@@ -9,7 +9,27 @@
  *    Stuart McCulloch (Sonatype, Inc.) - initial API and implementation
  *******************************************************************************/
 /**
- * Implements Plexus semantics on top of Google-Guice. 
+ * Implements Plexus semantics on top of Google-Guice.
+ * <p><p>
+ * Legacy applications can use the Plexus container API as usual:
+ * <p>
+ * <pre>
+ * ContainerConfiguration config = new DefaultContainerConfiguration();
+ * // ... configure ...
+ * PlexusContainer container = null;
+ * try {
+ *   container = new DefaultPlexusContainer( config );
+ *   // ... execute/wait ...
+ * } finally {
+ *   if ( container != null ) {
+ *     container.dispose();
+ *   }
+ * }</pre>
+ * 
+ * Sisu applications that want to re-use Plexus components can use the {@link org.eclipse.sisu.plexus.PlexusSpaceModule} wrapper:
+ * <p>
+ * <pre>
+ * binder.install( new PlexusSpaceModule( space ) );</pre>
  */
 package org.eclipse.sisu.plexus;
 
