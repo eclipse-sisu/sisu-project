98d6ffe0dc917bb89d1c5fa0fc8c94a3bd88a6bb
Stuart McCulloch <mcculls@gmail.com>
2013-07-19 03:24:26 +0100

    javadoc

--- org.eclipse.sisu.inject/src/org/eclipse/sisu/bean/package-info.java
+++ org.eclipse.sisu.inject/src/org/eclipse/sisu/bean/package-info.java
@@ -10,6 +10,21 @@
  *******************************************************************************/
 /**
  * Customizable injection of bean properties.
+ * <p><p>
+ * For example:
+ * 
+ * <pre>
+ * new AbstractModule() {
+ *   &#064;Override
+ *   protected void configure() {
+ *     bindListener( Matchers.any(), new BeanListener( new MyBeanBinder() ) );
+ *   }
+ * }</pre>
+ * MyBeanBinder will be asked to supply a {@link org.eclipse.sisu.bean.PropertyBinder} per-bean type.
+ * <p><p>
+ * Each PropertyBinder will in turn be asked for a {@link org.eclipse.sisu.bean.PropertyBinding} per-property.
+ * <p><p>
+ * The PropertyBindings are used to set properties in injected instances of the bean.
  */
 package org.eclipse.sisu.bean;
 
