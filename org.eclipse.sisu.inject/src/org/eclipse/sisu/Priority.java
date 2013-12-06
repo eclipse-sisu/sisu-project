ed17cfe015872dd589f8d0ed421524653b86e6d4
Stuart McCulloch <mcculls@gmail.com>
2013-12-06 01:04:41 +0000

    Bug 416556: add @Priority annotation

---
+++ org.eclipse.sisu.inject/src/org/eclipse/sisu/Priority.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * Copyright (c) 2010, 2013 Sonatype, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Stuart McCulloch (Sonatype, Inc.) - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.sisu;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Defines the priority ordering of a bean:<br>
+ * <br>
+ * 
+ * <pre>
+ * &#064;Named
+ * &#064;Priority( 999 )
+ * public class ImportantComponent
+ * {
+ *     //
+ * }
+ * </pre>
+ */
+@Target( value = { ElementType.TYPE } )
+@Retention( RetentionPolicy.RUNTIME )
+@Documented
+public @interface Priority
+{
+    int value();
+}
