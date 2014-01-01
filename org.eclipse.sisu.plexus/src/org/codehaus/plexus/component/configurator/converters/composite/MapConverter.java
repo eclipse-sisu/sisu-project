5b469c87c134ca9e78883981f138a8972ac31bdc
Stuart McCulloch <mcculls@gmail.com>
2014-01-01 22:33:53 +0000

    Bug 406688: allow maps of string to complex types in mojo configuration

--- org.eclipse.sisu.plexus/src/org/codehaus/plexus/component/configurator/converters/composite/MapConverter.java
+++ org.eclipse.sisu.plexus/src/org/codehaus/plexus/component/configurator/converters/composite/MapConverter.java
@@ -13,6 +13,7 @@
 package org.codehaus.plexus.component.configurator.converters.composite;
 
 import java.lang.reflect.Modifier;
+import java.lang.reflect.Type;
 import java.util.Map;
 import java.util.Properties;
 import java.util.TreeMap;
@@ -20,12 +21,15 @@ import java.util.TreeMap;
 import org.codehaus.plexus.component.configurator.ComponentConfigurationException;
 import org.codehaus.plexus.component.configurator.ConfigurationListener;
 import org.codehaus.plexus.component.configurator.converters.AbstractConfigurationConverter;
+import org.codehaus.plexus.component.configurator.converters.ConfigurationConverter;
+import org.codehaus.plexus.component.configurator.converters.ParameterizedConfigurationConverter;
 import org.codehaus.plexus.component.configurator.converters.lookup.ConverterLookup;
 import org.codehaus.plexus.component.configurator.expression.ExpressionEvaluator;
 import org.codehaus.plexus.configuration.PlexusConfiguration;
 
 public class MapConverter
     extends AbstractConfigurationConverter
+    implements ParameterizedConfigurationConverter
 {
     public boolean canConvert( final Class<?> type )
     {
@@ -37,6 +41,15 @@ public class MapConverter
                                      final ExpressionEvaluator evaluator, final ConfigurationListener listener )
         throws ComponentConfigurationException
     {
+        return fromConfiguration( lookup, configuration, type, null, enclosingType, loader, evaluator, listener );
+    }
+
+    public Object fromConfiguration( final ConverterLookup lookup, final PlexusConfiguration configuration,
+                                     final Class<?> type, final Type[] typeArguments, final Class<?> enclosingType,
+                                     final ClassLoader loader, final ExpressionEvaluator evaluator,
+                                     final ConfigurationListener listener )
+        throws ComponentConfigurationException
+    {
         final Object value = fromExpression( configuration, evaluator, type );
         if ( null != value )
         {
@@ -45,10 +58,23 @@ public class MapConverter
         try
         {
             final Map<Object, Object> map = instantiateMap( configuration, type, loader );
+            final Class<?> elementType = findElementType( typeArguments );
+            if ( Object.class == elementType || String.class == elementType )
+            {
+                for ( int i = 0, size = configuration.getChildCount(); i < size; i++ )
+                {
+                    final PlexusConfiguration element = configuration.getChild( i );
+                    map.put( element.getName(), fromExpression( element, evaluator ) );
+                }
+                return map;
+            }
+            // handle maps with complex element types...
+            final ConfigurationConverter converter = lookup.lookupConverterForType( elementType );
             for ( int i = 0, size = configuration.getChildCount(); i < size; i++ )
             {
                 final PlexusConfiguration element = configuration.getChild( i );
-                map.put( element.getName(), fromExpression( element, evaluator ) );
+                map.put( element.getName(), converter.fromConfiguration( lookup, element, elementType, enclosingType,
+                                                                         loader, evaluator, listener ) );
             }
             return map;
         }
@@ -76,5 +102,14 @@ public class MapConverter
         final Object impl = instantiateObject( implType );
         failIfNotTypeCompatible( impl, type, configuration );
         return (Map<Object, Object>) impl;
+    }
+
+    private static Class<?> findElementType( final Type[] typeArguments )
+    {
+        if ( null != typeArguments && typeArguments.length > 1 && typeArguments[1] instanceof Class<?> )
+        {
+            return (Class<?>) typeArguments[1];
+        }
+        return Object.class;
     }
 }
