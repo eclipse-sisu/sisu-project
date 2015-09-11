85ff8c04c1196152760fcba50e0cbf2779a8b5d9
Stuart McCulloch <mcculls@gmail.com>
2015-09-12 00:17:21 +0100

    Only call setAccessible if not already accessible

--- org.eclipse.sisu.inject/src/org/eclipse/sisu/space/QualifiedTypeBinder.java
+++ org.eclipse.sisu.inject/src/org/eclipse/sisu/space/QualifiedTypeBinder.java
@@ -315,7 +315,10 @@ public final class QualifiedTypeBinder
         {
             // slightly roundabout approach, but it might be private
             final Constructor<T> ctor = type.getDeclaredConstructor();
-            ctor.setAccessible( true );
+            if ( !ctor.isAccessible() )
+            {
+                ctor.setAccessible( true );
+            }
 
             // record this instance was created
             final T instance = ctor.newInstance();
