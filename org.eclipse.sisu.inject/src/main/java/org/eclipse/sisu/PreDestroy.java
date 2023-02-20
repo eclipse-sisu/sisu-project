45792377a0de5355356a923cf3809c3f5297118c
Tamas Cservenak <tamas@cservenak.net>
2023-02-20 12:24:38 +0100

    Fix headers

--- org.eclipse.sisu.inject/src/main/java/org/eclipse/sisu/PreDestroy.java
+++ org.eclipse.sisu.inject/src/main/java/org/eclipse/sisu/PreDestroy.java
@@ -1,13 +1,16 @@
-/*******************************************************************************
- * Copyright (c) 2010-present Sonatype, Inc.
+/********************************************************************************
+ * Copyright (c) 2023-present Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
- * Contributors:
- *   Stuart McCulloch (Sonatype, Inc.) - initial API and implementation
- *******************************************************************************/
+ * SPDX-License-Identifier: EPL-1.0
+ ********************************************************************************/
 package org.eclipse.sisu;
 
 import java.lang.annotation.Documented;
