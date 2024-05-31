8332fcefeb404f7745d43dfba5765ec5b2910d69
Tamas Cservenak <tamas@cservenak.net>
2024-05-31 18:31:22 +0200

    Licence change: EPL1 to EPL2 (#135)
    License change/update from EPL1 to EPL2.
    
    Refs:
    * https://www.eclipse.org/legal/epl-2.0/faq.php#epl-relicensing
    * https://gitlab.eclipse.org/eclipsefdn/emo-team/emo/-/issues/740#note_2309628
    
    Fixes #82

--- org.eclipse.sisu.inject/src/main/java/org/eclipse/sisu/wire/EntryListAdapter.java
+++ org.eclipse.sisu.inject/src/main/java/org/eclipse/sisu/wire/EntryListAdapter.java
@@ -1,13 +1,15 @@
-/*******************************************************************************
- * Copyright (c) 2010-present Sonatype, Inc.
+/*
+ * Copyright (c) 2010-2024 Sonatype, Inc.
  * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
+ * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
  *   Stuart McCulloch (Sonatype, Inc.) - initial API and implementation
- *******************************************************************************/
+ */
 package org.eclipse.sisu.wire;
 
 import java.util.AbstractSequentialList;
