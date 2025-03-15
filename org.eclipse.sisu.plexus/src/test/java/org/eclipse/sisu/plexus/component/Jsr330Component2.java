75ea2cc8201f54d248b73e0daf4cad84b853d51a
Konrad Windszus <konrad_w@gmx.de>
2025-03-15 20:11:37 +0100

    Add test for strict scanning

---
+++ org.eclipse.sisu.plexus/src/test/java/org/eclipse/sisu/plexus/component/Jsr330Component2.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Konrad Windszus - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.sisu.plexus.component;
+
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+@Named
+@Singleton
+public class Jsr330Component2
+{
+    public Jsr330Component2()
+    {
+    }
+}
