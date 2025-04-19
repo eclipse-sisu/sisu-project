d49f9bbd2d58ba8c143cacc316838a683d99ddae
Tamas Cservenak <tamas@cservenak.net>
2025-04-19 23:06:50 +0200

    More coverage (#195)

--- org.eclipse.sisu.plexus/src/test/java/org/codehaus/plexus/component/configurator/converters/composite/PropertiesConverterTest.java
+++ org.eclipse.sisu.plexus/src/test/java/org/codehaus/plexus/component/configurator/converters/composite/PropertiesConverterTest.java
@@ -4,7 +4,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *******************************************************************************/
-package org.codehaus.plexus.component.configurator.converters.basic;
+package org.codehaus.plexus.component.configurator.converters.composite;
 
 import org.codehaus.plexus.component.configurator.ComponentConfigurationException;
 import org.codehaus.plexus.component.configurator.expression.ExpressionEvaluationException;
@@ -13,33 +13,35 @@ import org.codehaus.plexus.configuration.xml.XmlPlexusConfiguration;
 import org.junit.Test;
 
 import java.io.File;
-import java.nio.file.StandardCopyOption;
+import java.util.Properties;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-public class EnumConverterTest
+public class PropertiesConverterTest
 {
     @Test
     public void canConvertPositive()
     {
-        assertTrue( new EnumConverter().canConvert( StandardCopyOption.class ) );
+        assertTrue( new PropertiesConverter().canConvert( Properties.class ) );
     }
 
     @Test
     public void canConvertNegative()
     {
-        assertFalse( new EnumConverter().canConvert( Object.class ) );
+        assertFalse( new PropertiesConverter().canConvert( Object.class ) );
     }
 
     @Test
     public void testConvert()
         throws ComponentConfigurationException
     {
-        XmlPlexusConfiguration config = new XmlPlexusConfiguration( "field" );
-        config.setValue( StandardCopyOption.ATOMIC_MOVE.name() );
-        Object object = new EnumConverter().fromConfiguration( null, config, StandardCopyOption.class, null, null,
+        XmlPlexusConfiguration config = new XmlPlexusConfiguration( "properties" );
+        config.addChild("key1", "value1" );
+        config.addChild( "key2", "value2" );
+
+        Object object = new PropertiesConverter().fromConfiguration( null, config, Properties.class, null, null,
                                                                new ExpressionEvaluator()
                                                                {
                                                                    @Override
@@ -55,7 +57,10 @@ public class EnumConverterTest
                                                                        return null;
                                                                    }
                                                                }, null );
-        assertTrue( object instanceof StandardCopyOption );
-        assertEquals( StandardCopyOption.ATOMIC_MOVE, object );
+        assertTrue( object instanceof Properties );
+        Properties result = (Properties) object;
+        assertTrue( result.size() == 2 );
+        assertEquals( "value1", result.getProperty("key1") );
+        assertEquals( "value2", result.getProperty("key2") );
     }
 }
