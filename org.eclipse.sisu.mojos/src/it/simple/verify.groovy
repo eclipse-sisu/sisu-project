44a9f3e3b2284790894d9b6586d771bb09c5bc0b
Tamas Cservenak <tamas@cservenak.net>
2025-05-09 21:22:52 +0200

    Simplify assertions

--- org.eclipse.sisu.mojos/src/it/simple/verify.groovy
+++ org.eclipse.sisu.mojos/src/it/simple/verify.groovy
@@ -1,7 +1,5 @@
 // Basic IT: index is created and contains one class FQN
 
-assert new File(basedir, 'target/classes').isDirectory()
-assert new File(basedir, 'target/classes/META-INF/sisu').isDirectory()
 File index = new File(basedir, 'target/classes/META-INF/sisu/javax.inject.Named');
 assert index.isFile()
 assert index.text.contains('simple.Test')
